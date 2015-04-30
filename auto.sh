c3632c1bd6bfbcb82d9fecfcb0819817e37facef
Yosefa Ferdianto <yosefa@email.mm-unlam.ac.id>
2015-04-30 08:18:37 +0700

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,45 @@
+rm -rf un.sh*
+wget https://github.com/yosefa/vps/raw/master/add
+wget https://github.com/yosefa/vps/raw/master/cari
+wget https://github.com/yosefa/vps/raw/master/crot
+wget https://github.com/yosefa/vps/raw/master/hapus
+wget https://github.com/yosefa/vps/raw/master/lagi
+wget https://github.com/yosefa/vps/raw/master/lihat
+wget https://github.com/yosefa/vps/raw/master/tambah
+wget https://github.com/yosefa/vps/raw/master/kurang
+wget https://github.com/yosefa/vps/raw/master/trial
+wget https://github.com/yosefa/vps/raw/master/masuk
+wget https://github.com/yosefa/vps/raw/master/expuser
+wget https://github.com/yosefa/vps/raw/master/un.sh
+wget https://github.com/yosefa/vps/raw/master/otomatis
+wget https://github.com/yosefa/vps/raw/master/bersih
+chmod +x add
+chmod +x cari
+chmod +x crot
+chmod +x hapus
+chmod +x lagi
+chmod +x lihat
+chmod +x tambah
+chmod +x kurang
+chmod +x trial
+chmod +x masuk
+chmod +x expuser
+chmod +x un.sh
+chmod +x otomatis
+chmod +x bersih
+apt-get install dos2unix
+dos2unix add
+dos2unix cari
+dos2unix crot
+dos2unix hapus
+dos2unix lagi
+dos2unix lihat
+dos2unix tambah
+dos2unix kurang
+dos2unix trial
+dos2unix masuk
+dos2unix expuser
+dos2unix un.sh
+dos2unix otomatis
+dos2unix bersih
+rm -rf auto.sh
